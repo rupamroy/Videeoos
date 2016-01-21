6c43ffb8bff87dcb3e5bc2b9fe189cc84ab45600
rupamroy <roopamroy@gmail.com>
2016-01-20 23:00:20 -0500

    Home page work started

---
+++ src/client/js/features/home/home.controller.js
@@ -0,0 +1,10 @@
+(function(){
+     angular.module('videeooApp')
+         .controller('home', HomeController);
+
+     HomeController.$inject=['$scope'];
+
+     function HomeController($scope){
+        $scope.content="This is a test content";
+     }
+})();
